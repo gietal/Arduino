058b6842c95ad8b3f45a85151d9396d09d0e95dd
gietal <gieta.laksmana@hotmail.com>
2016-01-19 20:53:53 -0800

    4 legs initial positions

--- Personal/Spiderbot/SpiderLeg/SpiderLeg/SpiderLeg.ino
+++ Personal/Spiderbot/SpiderLeg/SpiderLeg/SpiderLeg.ino
@@ -19,25 +19,82 @@
 //int joint1Neutral = 30;
 //int joint1Up = 140;
 
-SpiderLegController leg;
+SpiderLegController legs[4];
 
 // the setup function runs once when you press reset or power the board
+
+void SetFrontRight(SpiderLegController *pLeg)
+{
+	// right leg 
+	pLeg->SetDownDegree(0, 130);
+	pLeg->SetNeutralDegree(0, 90);
+	pLeg->SetUpDegree(0, 50);
+
+	pLeg->SetDownDegree(1, 180);
+	pLeg->SetNeutralDegree(1, 90);
+	pLeg->SetUpDegree(1, 40);
+
+	pLeg->SetDownDegree(2, 20);
+	pLeg->SetNeutralDegree(2, 50);
+	pLeg->SetUpDegree(2, 140);
+}
+
+void SetBackRight(SpiderLegController *pLeg)
+{
+	// right leg 
+	pLeg->SetDownDegree(0, 30);
+	pLeg->SetNeutralDegree(0, 80);
+	pLeg->SetUpDegree(0, 120);
+
+	pLeg->SetDownDegree(1, 180);
+	pLeg->SetNeutralDegree(1, 60);
+	pLeg->SetUpDegree(1, 40);
+
+	pLeg->SetDownDegree(2, 20);
+	pLeg->SetNeutralDegree(2, 50);
+	pLeg->SetUpDegree(2, 140);
+}
+
+void SetFrontLeft(SpiderLegController *pLeg)
+{
+	pLeg->SetDownDegree(0, 120);
+	pLeg->SetNeutralDegree(0, 80);
+	pLeg->SetUpDegree(0, 50);
+
+	pLeg->SetDownDegree(1, 80);
+	pLeg->SetNeutralDegree(1, 100);
+	pLeg->SetUpDegree(1, 150);
+
+	pLeg->SetDownDegree(2, 90);
+	pLeg->SetNeutralDegree(2, 50);
+	pLeg->SetUpDegree(2, 0);
+}
+
+void SetBackLeft(SpiderLegController *pLeg)
+{
+	pLeg->SetDownDegree(0, 120);
+	pLeg->SetNeutralDegree(0, 70);
+	pLeg->SetUpDegree(0, 50);
+
+	pLeg->SetDownDegree(1, 80);
+	pLeg->SetNeutralDegree(1, 90);
+	pLeg->SetUpDegree(1, 150);
+
+	pLeg->SetDownDegree(2, 100);
+	pLeg->SetNeutralDegree(2, 90);
+	pLeg->SetUpDegree(2, 0);
+}
 void setup() 
 {
-	leg.Attach(11, 9, 10);
+	legs[0].Attach(2, 3, 4); // front left
+	legs[1].Attach(5, 6, 7); // back left
+	legs[2].Attach(8, 9, 10); // back right 
+	legs[3].Attach(11, 12, 13); // front right
 
-	//horizontal axis
-	leg.SetDownDegree(0, 130);
-	leg.SetNeutralDegree(0, 90);
-	leg.SetUpDegree(0, 50);
-
-	leg.SetDownDegree(1, 180);
-	leg.SetNeutralDegree(1, 90);
-	leg.SetUpDegree(1, 40);
-
-	leg.SetDownDegree(2, 20);
-	leg.SetNeutralDegree(2, 50);
-	leg.SetUpDegree(2, 140);
+	SetFrontLeft(legs + 0);
+	SetBackLeft(legs + 1);
+	SetBackRight(legs + 2);
+	SetFrontRight(legs + 3);
 
 	//leg.GoNeutral();
 	//delay(1000);
@@ -48,24 +105,27 @@ void setup()
 	//delay(1000);
 	//leg.GoDown();
 
-	leg.Init();
-	delay(1000);
+	for (int i = 0; i < 4; ++i)
+		legs[i].Init();
+	delay(500);
+
 	
-	// opening
-	leg.GotoAngle(0, 30);
-	delay(500);
-	leg.GotoAngle(1, 0);
-	delay(500);
-	for (int i = 0; i < 2; ++i)
-	{
-		leg.GotoAngle(2, 150);
-		delay(100);
-		leg.GotoAngle(2, 20);
-		delay(100);
-	}
-	leg.GoNeutral(2);
-	leg.GoNeutral();
-	delay(1000);
+
+	//// opening
+	//leg.GotoAngle(0, 30);
+	//delay(500);
+	//leg.GotoAngle(1, 0);
+	//delay(500);
+	//for (int i = 0; i < 2; ++i)
+	//{
+	//	leg.GotoAngle(2, 150);
+	//	delay(100);
+	//	leg.GotoAngle(2, 20);
+	//	delay(100);
+	//}
+	//leg.GoNeutral(2);
+	//leg.GoNeutral();
+	//delay(1000);
 	//WalkCycle();
 	//leg.Init();
 	//delay(500);
@@ -73,33 +133,43 @@ void setup()
 	//delay(500);
 	//leg.GoNeutral(2);
 	//leg.GoNeutral(1);
+	
+	for (int i = 0; i < 4; ++i)
+	{
+		WalkCycle(legs + i);
+		delay(100);
+	}
+}
+// the loop function runs over and over again until power down or reset
+void loop()
+{
+	//for (int i = 0; i < 4; ++i)
+	//{
+	//	WalkCycle(legs + i);
+	//	delay(100);
+	//}
 }
 
-
 // blocking synchronous walk cycle
-void WalkCycle()
+void WalkCycle(SpiderLegController *pLeg)
 {
 	int waitTime = 50;
 
 	// horizonta axis go back
-	leg.GoDown(0);
+	pLeg->GoDown(0);
 	delay(waitTime);
 
 	// vertical axis go up
-	leg.GoUp(1);
+	pLeg->GoUp(1);
 	delay(waitTime);
 
 	// horizontal axis go forward
-	leg.GoUp(0);
+	pLeg->GoUp(0);
 	delay(waitTime);
 
 	// vertical axis go down
-	leg.GoNeutral(1);
+	pLeg->GoNeutral(1);
 	delay(waitTime);
 }
 
-// the loop function runs over and over again until power down or reset
-void loop() 
-{
-	WalkCycle();
-}
+
